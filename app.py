f93f115fafcb6d21e01fa8430058a2c86056fe80
Emin-cc <aydin.eminb@gmail.com>
2025-04-14 15:14:38 -0400

    good code change

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def home():
       <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>DevOps Showcase - Bhargav</title>
+        <title>Hello from  Emin Aydin App</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
         <style>
           body {{
